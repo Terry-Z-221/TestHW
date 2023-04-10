14a6451bd60b84fb1f71155565a8cd0b15522564
zengmz0221 <974378437@qq>
2023-04-10 22:08:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/jnu/edu/x2020101336/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.jnu.edu.x2020101336;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
