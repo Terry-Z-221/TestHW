697fd401c67b85fd9f5a8f58b7ed3d5e09fcb845
zengmz0221 <974378437@qq>
2023-04-11 16:49:41 +0800

    在activity_main中添加了三个组件，通过Recode Espresso Test建立驱动接受测试，在MainActivity中添加按钮的响应函数以及数据处理，app可正常运行，数据正确，但测试里遇到了与老师课上出现的问题。

--- app/src/main/java/cn/jnu/edu/x2020101336/MainActivity.java
+++ app/src/main/java/cn/jnu/edu/x2020101336/MainActivity.java
@@ -3,6 +3,10 @@ package cn.jnu.edu.x2020101336;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_cal = this.findViewById(R.id.Calculate);
+        EditText edit_score = this.findViewById(R.id.edittext_score);
+        TextView text_score = this.findViewById(R.id.score);
+
+        btn_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String score = edit_score.getText().toString();
+                String []arrayScore = score.split(",");
+                BowlingGame game = new BowlingGame();
+                for (int i = 0; i < arrayScore.length; i++)
+                {
+                    game.roll(Integer.parseInt(arrayScore[i]));
+                }
+                text_score.setText("The total score:" + game.score());
+
+            }
+        });
     }
 }
